224ea9fabc11082d45cf7c8818342a6888368378
andreasaker <andreas.aker91@gmail.com>
2020-03-09 14:43:16 +0100

    Authentication now hides Admin interface

--- src/Admin.js
+++ src/Admin.js
@@ -5,8 +5,9 @@ import AddCoursesForm from "./components/AddCourseForm";
 import CategoryList from "./components/CategoryList";
 import AddCategoryForm from "./components/AddCategoryForm";
 import Authentication from "./components/Authentication";
+import { connect } from "react-redux";
 
-export default function Admin() {
+const Admin = ({ loggedIn }) => {
   return (
     <div className="App">
       <div className="head_block">
@@ -14,14 +15,23 @@ export default function Admin() {
         <h2>Start adding food!</h2>
       </div>
       <div className="content_block">
-        <Authentication />
-        <AddCoursesForm />
-        <AddCategoryForm />
-        <CategoryList />
+        {loggedIn ? (
+          <div>
+            <AddCoursesForm />
+            <AddCategoryForm />
+            <CategoryList />
+          </div>
+        ) : (
+          <Authentication />
+        )}
       </div>
       <div className="footer_block">
         <Footer />
       </div>
     </div>
   );
-}
+};
+
+const mapStatetoProps = state => ({ loggedIn: state.loggedIn });
+
+export default connect(mapStatetoProps)(Admin);
--- src/components/Authentication.js
+++ src/components/Authentication.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import { loggedIn } from "../redux/actions";
 
-const Authentication = () => {
+const Authentication = ({ setStatus }) => {
+  const loginInit = { username: "", password: "" };
+  const [loginInfo, setLoginInfo] = useState(loginInit);
+
+  const handleChange = e => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    setLoginInfo({ ...loginInfo, [name]: value });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setStatus(true);
+    setLoginInfo(loginInit);
+  };
+
   return (
-    <form>
-      <input type="text" name="username" />
-      <input type="password" name="password" />
+    <form onSubmit={e => handleSubmit(e)}>
+      <input
+        type="text"
+        name="username"
+        value={loginInfo.username}
+        onChange={e => handleChange(e)}
+      />
+      <input
+        type="password"
+        name="password"
+        value={loginInfo.password}
+        onChange={e => handleChange(e)}
+      />
       <button>Login</button>
     </form>
   );
